e217c1254682931e33261d3e98f0d180d5f1a800
Ian <ian_frye@outlook.com>
2024-05-14 06:47:08 -0400

    working on setting up appwrite

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,4 +1,6 @@
 'use server';
+import { account, database } from '../appwriteConfig';
+import { ID } from 'appwrite';
 
 export async function signIn(email: string, password: string) {
   try {
@@ -9,6 +11,30 @@ export async function signIn(email: string, password: string) {
 
 export async function signUp(userData: SignUpParams) {
   try {
+    const response = await account.create(
+      ID.unique(),
+      userData.email,
+      userData.password,
+      userData.firstName
+    );
+
+    const dbResponse = await database.createDocument(
+      process.env.APPWRITE_DATABASE_ID,
+      process.env.APPWRITE_USER_COLLECTION_ID,
+      ID.unique(),
+      {
+        userId: response.$id,
+        dwollaCustomerUrl: '',
+        dwollaCustomerId: '',
+        firstName: userData.firstName,
+        lastName: userData.lastName,
+        address1: userData.address1,
+        city: userData.city,
+        postalCode: userData.postalCode,
+        dateOfBirth: userData.dateOfBirth,
+        ssn: userData.ssn,
+      }
+    );
   } catch (error) {
     console.error(error);
   }
